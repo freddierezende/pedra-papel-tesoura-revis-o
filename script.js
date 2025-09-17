d6a9d1870a610e34b002f722924a1fdfe84d2b2f
Freddierezende <freddie.rezende.2003@gmail.com>
2025-09-17 16:23:54 -0300

    Finalizado

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-/* Desenvolva seu código abaixo */
+function playRockPaperScissor(player1, player2) {
+    const pedra = 'Pedra';
+    const papel = 'Papel';
+    const tesoura = 'Tesoura';
+
+    if (player1 === player2) {
+        return 'Empate!';
+    }
+    
+    else if ((player1 === pedra && player2 === tesoura) || (player1 === papel && player2 === pedra) || 
+    (player1 === tesoura && player2 === papel)) {
+        return 'Jogador 1 venceu!';
+    }
+   
+    return 'Jogador 2 venceu!';
+
+    
+}
